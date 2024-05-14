04a4d4574aa324706ff44c93ebb02c081764e47a
Nikita <kniki098@gmail.com>
2024-05-14 23:47:20 +0530

    Commit changes

--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -9,6 +9,10 @@ CATEGORY = (
 )
 
 class Product(models.Model):
-    name = models.CharField(max_length=100)
-    category = models.CharField(max_length=20)
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length=100, null = True)
+    category = models.CharField(max_length=20, choices = CATEGORY, null = True)
+    quantity = models.PositiveIntegerField(null = True)
+
+
+    def __str__(self):
+        return f'{self.name}-{self.quantity}'
